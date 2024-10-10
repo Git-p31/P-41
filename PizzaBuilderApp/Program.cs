79a8ecb59f6047b83d959f6c65baca5ecdaeb680
Git-p31 <lolpoking18@gmail.com>
2024-10-10 08:50:25 +0300

    0

---
+++ PizzaBuilderApp/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+
+namespace PizzaBuilderApp
+{
+    // Класс продукта
+    class Pizza
+    {
+        public string Dough { get; set; }
+        public string Sauce { get; set; }
+        public List<string> Toppings { get; set; } = new List<string>();
+
+        public void ShowPizza()
+        {
+            MessageBox.Show($"Пицца с тестом: {Dough}, соусом: {Sauce}, начинками: {string.Join(", ", Toppings)}", "Собранная пицца");
+        }
+    }
+
+    // Интерфейс строителя
+    interface IPizzaBuilder
+    {
+        void SetDough();
+        void SetSauce();
+        void SetToppings();
+        Pizza GetPizza();
+    }
+
+    // Конкретный строитель для Маргариты
+    class MargheritaPizzaBuilder : IPizzaBuilder
+    {
+        private Pizza _pizza = new Pizza();
+
+        public void SetDough() => _pizza.Dough = "Тонкое тесто";
+        public void SetSauce() => _pizza.Sauce = "Томатный соус";
+        public void SetToppings() => _pizza.Toppings.AddRange(new List<string> { "Сыр Моцарелла", "Базилик" });
+
+        public Pizza GetPizza() => _pizza;
+    }
+
+    // Конкретный строитель для Пепперони
+    class PepperoniPizzaBuilder : IPizzaBuilder
+    {
+        private Pizza _pizza = new Pizza();
+
+        public void SetDough() => _pizza.Dough = "Толстое тесто";
+        public void SetSauce() => _pizza.Sauce = "Соус BBQ";
+        public void SetToppings() => _pizza.Toppings.AddRange(new List<string> { "Сыр Чеддер", "Пепперони" });
+
+        public Pizza GetPizza() => _pizza;
+    }
+
+    // Директор, управляющий строителем
+    class PizzaDirector
+    {
+        private IPizzaBuilder _pizzaBuilder;
+
+        public void SetPizzaBuilder(IPizzaBuilder pizzaBuilder)
+        {
+            _pizzaBuilder = pizzaBuilder;
+        }
+
+        public void ConstructPizza()
+        {
+            _pizzaBuilder.SetDough();
+            _pizzaBuilder.SetSauce();
+            _pizzaBuilder.SetToppings();
+        }
+
+        public Pizza GetPizza() => _pizzaBuilder.GetPizza();
+    }
+
+    // Класс формы приложения
+    public class PizzaForm : Form
+    {
+        private ComboBox pizzaTypeComboBox;
+        private Button buildButton;
+        private TextBox outputTextBox;
+
+        public PizzaForm()
+        {
+            // Настройка формы
+            this.Text = "Пицца Строитель";
+            this.Width = 400;
+            this.Height = 350;
+
+            // Элементы управления
+            pizzaTypeComboBox = new ComboBox { Left = 50, Top = 20, Width = 280 };
+            pizzaTypeComboBox.Items.AddRange(new string[] { "Маргарита", "Пепперони" });
+            pizzaTypeComboBox.SelectedIndex = 0;
+
+            buildButton = new Button { Left = 50, Top = 60, Width = 280, Text = "Собрать пиццу" };
+            buildButton.Click += BuildButton_Click;
+
+            outputTextBox = new TextBox { Left = 50, Top = 100, Width = 280, Height = 180, Multiline = true, ReadOnly = true };
+
+            // Добавление элементов на форму
+            Controls.Add(pizzaTypeComboBox);
+            Controls.Add(buildButton);
+            Controls.Add(outputTextBox);
+        }
+
+        private void BuildButton_Click(object sender, EventArgs e)
+        {
+            string pizzaType = pizzaTypeComboBox.SelectedItem?.ToString();
+            if (pizzaType == null)
+            {
+                outputTextBox.Text = "Пожалуйста, выберите тип пиццы.";
+                return;
+            }
+
+            PizzaDirector director = new PizzaDirector();
+            IPizzaBuilder pizzaBuilder = null;
+
+            switch (pizzaType)
+            {
+                case "Маргарита":
+                    pizzaBuilder = new MargheritaPizzaBuilder();
+                    break;
+                case "Пепперони":
+                    pizzaBuilder = new PepperoniPizzaBuilder();
+                    break;
+                default:
+                    outputTextBox.Text = "Неизвестный тип пиццы.";
+                    return;
+            }
+
+            director.SetPizzaBuilder(pizzaBuilder);
+            outputTextBox.Text = "Начинаем сборку пиццы...\r\n";
+
+            // Показываем пошаговое выполнение паттерна
+            outputTextBox.AppendText("1. Замешиваем тесто...\r\n");
+            pizzaBuilder.SetDough();
+
+            outputTextBox.AppendText("2. Добавляем соус...\r\n");
+            pizzaBuilder.SetSauce();
+
+            outputTextBox.AppendText("3. Добавляем начинки...\r\n");
+            pizzaBuilder.SetToppings();
+
+            outputTextBox.AppendText("4. Завершаем сборку пиццы.\r\n");
+
+            Pizza pizza = director.GetPizza();
+            outputTextBox.AppendText($"Пицца собрана: {pizza.Dough} тесто, {pizza.Sauce} соус, начинки: {string.Join(", ", pizza.Toppings)}.");
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new PizzaForm());
+        }
+    }
+}
---
+++ PizzaPrototypeApp/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PizzaPrototypeApp
+{
+    // Абстрактный класс прототипа для пиццы
+    public abstract class PizzaPrototype
+    {
+        public string Dough { get; set; }
+        public string Sauce { get; set; }
+        public List<string> Toppings { get; set; } = new List<string>();
+
+        // Абстрактный метод клонирования
+        public abstract PizzaPrototype Clone();
+
+        public override string ToString()
+        {
+            return $"Pizza with {Dough} dough, {Sauce} sauce, toppings: {string.Join(", ", Toppings)}";
+        }
+    }
+
+    // Конкретная пицца Маргарита с возможностью клонирования
+    public class MargheritaPizza : PizzaPrototype
+    {
+        public MargheritaPizza()
+        {
+            Dough = "Thin crust";
+            Sauce = "Tomato";
+            Toppings.Add("Mozzarella");
+            Toppings.Add("Basil");
+        }
+
+        // Реализация метода клонирования
+        public override PizzaPrototype Clone()
+        {
+            // Глубокое копирование
+            MargheritaPizza clone = (MargheritaPizza)this.MemberwiseClone();
+            clone.Toppings = new List<string>(this.Toppings); // Клонирование начинки
+            return clone;
+        }
+    }
+
+    // Конкретная пицца Пепперони с возможностью клонирования
+    public class PepperoniPizza : PizzaPrototype
+    {
+        public PepperoniPizza()
+        {
+            Dough = "Thick crust";
+            Sauce = "BBQ";
+            Toppings.Add("Mozzarella");
+            Toppings.Add("Pepperoni");
+        }
+
+        public override PizzaPrototype Clone()
+        {
+            PepperoniPizza clone = (PepperoniPizza)this.MemberwiseClone();
+            clone.Toppings = new List<string>(this.Toppings);
+            return clone;
+        }
+    }
+
+    public class PizzaForm : Form
+    {
+        private ComboBox pizzaTypeComboBox;
+        private Button buildButton;
+        private Button cloneButton;
+        private TextBox outputTextBox;
+        private PizzaPrototype currentPizza;
+
+        public PizzaForm()
+        {
+            // Настройка формы
+            this.Text = "Pizza Prototype";
+            this.Width = 400;
+            this.Height = 400;
+
+            // Элементы управления
+            pizzaTypeComboBox = new ComboBox { Left = 50, Top = 20, Width = 280 };
+            pizzaTypeComboBox.Items.AddRange(new string[] { "Маргарита", "Пепперони" });
+            pizzaTypeComboBox.SelectedIndex = 0;
+
+            buildButton = new Button { Left = 50, Top = 60, Width = 280, Text = "Создать пиццу" };
+            buildButton.Click += BuildButton_Click;
+
+            cloneButton = new Button { Left = 50, Top = 100, Width = 280, Text = "Клонировать пиццу" };
+            cloneButton.Click += CloneButton_Click;
+
+            outputTextBox = new TextBox { Left = 50, Top = 140, Width = 280, Height = 200, Multiline = true, ReadOnly = true };
+
+            // Добавление элементов на форму
+            Controls.Add(pizzaTypeComboBox);
+            Controls.Add(buildButton);
+            Controls.Add(cloneButton);
+            Controls.Add(outputTextBox);
+        }
+
+        private void BuildButton_Click(object sender, EventArgs e)
+        {
+            string pizzaType = pizzaTypeComboBox.SelectedItem.ToString();
+            if (pizzaType == "Маргарита")
+            {
+                currentPizza = new MargheritaPizza();
+            }
+            else if (pizzaType == "Пепперони")
+            {
+                currentPizza = new PepperoniPizza();
+            }
+
+            outputTextBox.Text = $"Создана пицца: {currentPizza}\r\n";
+        }
+
+        private void CloneButton_Click(object sender, EventArgs e)
+        {
+            if (currentPizza != null)
+            {
+                PizzaPrototype clonedPizza = currentPizza.Clone();
+                outputTextBox.AppendText($"Клонированная пицца: {clonedPizza}\r\n");
+            }
+            else
+            {
+                outputTextBox.Text = "Сперва создайте пиццу.";
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new PizzaForm());
+        }
+    }
+}
